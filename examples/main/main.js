f9685020697f6278296e2a388c7eaf01c650c9f2
Rodney Thomas <r7lrodney@outlook.com>
2025-10-28 14:55:18 -0500

    Refactor main.js to import modules and remove old code

--- examples/main/main.js
+++ examples/main/main.js
@@ -1,28 +1,5 @@
-
-// Initialize the Engine and canvas.
-const canvas = document.getElementById('game');
-const engine = new Engine({canvas, width: 800, height: 600, pixelRatio: 1});
-
-// Create Box Entity
-class Box extends Entity {
-  constructor(x,y){
-    super({
-      pos:{x,y},
-      size:{w:48,h:48},
-      color:'#f90'
-    })
-  }
-}
-
-// Initialize New Scene
-const scene = new Scene();
-
-// Add a box to the scene (previously defined)
-scene.add(new Box(300, 200);
-
-engine.setScene(scene);
-engine.start();
-
-// expose for dev console
-window.cap = {engine, scene};
-
+// Import All Required Modules
+import Engine from 'https://raw.githubusercontent.com/r7l-labs/cap-engine/refs/heads/main/src/capjs/engine.js';
+import { Scene } from 'https://raw.githubusercontent.com/r7l-labs/cap-engine/refs/heads/main/src/capjs/scene.js';
+import { Entity } from 'https://raw.githubusercontent.com/r7l-labs/cap-engine/refs/heads/main/src/capjs/entity.js';
+import { Vec2 } from 'https://raw.githubusercontent.com/r7l-labs/cap-engine/refs/heads/main/src/capjs/math.js';
