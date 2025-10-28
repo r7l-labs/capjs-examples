9bb4e6d9220c5fa02c66674a9080f5cf1b460c84
Rodney Thomas <r7lrodney@outlook.com>
2025-10-28 14:56:26 -0500

    Update main.js

--- examples/main/main.js
+++ examples/main/main.js
@@ -1,5 +1,7 @@
-// Import All Required Modules
+// Import All Required Modules from Engine
 import Engine from 'https://raw.githubusercontent.com/r7l-labs/cap-engine/refs/heads/main/src/capjs/engine.js';
 import { Scene } from 'https://raw.githubusercontent.com/r7l-labs/cap-engine/refs/heads/main/src/capjs/scene.js';
 import { Entity } from 'https://raw.githubusercontent.com/r7l-labs/cap-engine/refs/heads/main/src/capjs/entity.js';
 import { Vec2 } from 'https://raw.githubusercontent.com/r7l-labs/cap-engine/refs/heads/main/src/capjs/math.js';
+
+const canvas = document.getElementById('game');
